3d7a118e734e0c8a554d0279067c83cc24ee2b4e
sadagatasgarov <sedaqet.esgerov@gmail.com>
2020-07-27 00:52:11 +0400

    almost ready

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ let number = prompt('sayi girin');
 
 
 const container = document.querySelector('#container');
-container.style='justify-conteny:center; text-align: center;top: 0px;left: 0px;width: 100%;height: 100%;padding: 2px 0px 0px;background: radial-gradient(circle, rgb(255, 75, 75) 0%, rgb(218, 63, 63) 100%)';
+container.style='display:flex;  flex-direction:column;justify-conteny:center; text-align: center;top: 0px;left: 0px;width: 100%;height: 100%;padding: 2px 0px 0px;background: radial-gradient(circle, rgb(255, 75, 75) 0%, rgb(218, 63, 63) 100%)';
 const button = document.createElement('button');
 const divElement1 = document.createElement('div');
 divElement1.className = 'divElement1'
@@ -14,11 +14,11 @@ divElement1.className = 'divElement2';
 container.appendChild(divElement2);
 
 function divCizim(number) {
-    divElement2.style = `background: white;margin:20%;display: grid;grid-template-columns: repeat(${number}, minmax(16px,auto));grid-template-rows: repeat(${number}, minmax(16px,auto));`
+    divElement2.style = `position:relative;margin:20%;display: grid;grid-template-columns: repeat(${number}, minmax(16px,auto));grid-template-rows: repeat(${number}, minmax(16px,auto));`
     for (let i = 0; i < number * number; i++) {
         const div = document.createElement('div');
         div.className = 'divElement'
-        div.style = ' position: relative; width:auto ;border: 2px solid red; height: auto;'
+        div.style = 'width:auto ;border: 2px solid red; height: auto;background: white'
         divElement2.appendChild(div);
     }
 }
