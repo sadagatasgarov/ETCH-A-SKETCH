c8d0eae3be250e236a590901bf9da6d314c2274e
sadagatasgarov <67124550+sadagatasgarov@users.noreply.github.com>
2020-07-27 00:17:31 +0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,34 @@
+
+let number = prompt('sayi girin');
+
+
+const container = document.querySelector('#container');
+container.style='justify-conteny:center; text-align: center;top: 0px;left: 0px;width: 100%;height: 100%;padding: 2px 0px 0px;background: radial-gradient(circle, rgb(255, 75, 75) 0%, rgb(218, 63, 63) 100%)';
+const button = document.createElement('button');
+const divElement1 = document.createElement('div');
+divElement1.className = 'divElement1'
+divElement1.appendChild(button);
+container.appendChild(divElement1);
+const divElement2 = document.createElement('div');
+divElement1.className = 'divElement2';
+container.appendChild(divElement2);
+
+function divCizim(number) {
+    divElement2.style = `background: white;margin:20%;display: grid;grid-template-columns: repeat(${number}, minmax(16px,auto));grid-template-rows: repeat(${number}, minmax(16px,auto));`
+    for (let i = 0; i < number * number; i++) {
+        const div = document.createElement('div');
+        div.className = 'divElement'
+        div.style = ' position: relative; width:auto ;border: 2px solid red; height: auto;'
+        divElement2.appendChild(div);
+    }
+}
+divCizim(number);
+
+const divselect = document.querySelectorAll('.divElement');
+divselect.forEach((div) => {
+    div.addEventListener('click', () => { div.style = 'background-color: blueviolet' });
+    div.addEventListener('mouseover', () => { div.style = 'background-color: pink' });
+    div.addEventListener('mouseover', () => { div.style = 'background-color: yellow' });
+    div.addEventListener('mousedown', () => { div.style = 'background-color: black' });
+    div.addEventListener('mouseup', () => { div.style = 'background-color: red' });
+});
